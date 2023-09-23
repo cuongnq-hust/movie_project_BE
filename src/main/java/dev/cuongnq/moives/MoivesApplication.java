4b8230d1e48f8b6804961de50e9843dbda01cbfb
cuong.nguyen <cuongnq.hust@gmail.com>
2023-09-23 18:29:12 +0700

    update BE

---
+++ src/main/java/dev/cuongnq/moives/MoivesApplication.java
@@ -0,0 +1,31 @@
+package dev.cuongnq.moives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class MoivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoivesApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+
+}
---
+++ src/test/java/dev/cuongnq/moives/MoivesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cuongnq.moives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
